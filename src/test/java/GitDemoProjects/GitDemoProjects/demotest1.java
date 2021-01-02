9f4611761181cc2bb65d03dfd5302b906899cbdb
Piyush gupta12 <Piyush_Gupta12@ad.infosys.com>
2021-01-02 17:25:00 +0530

    second commit

---
+++ src/test/java/GitDemoProjects/GitDemoProjects/demotest1.java
@@ -0,0 +1,11 @@
+package GitDemoProjects.GitDemoProjects;
+
+public class demotest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("in demo test2");
+
+	}
+
+}
